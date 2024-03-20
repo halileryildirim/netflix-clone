978f7b4d57500c420e042157a21c091d6b4513b7
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-21 02:24:59 +0300

    Add summary and button containers for banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,6 +6,14 @@ const Banner = () => {
             <div className="billboard-logo">
                 <img src="https://occ-0-7290-2706.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABS1kQ3bLw61sI8nNQHftRAKuEw-TfNIAjZ2UnZKfwlSWMuc7JVGnvJiFqvkem_5adGMUswsUoJjL0zyIkyIK2QS7Cah82EJv7CKrzZDkapM.webp?r=c0d" />
             </div>
+            <div className="summary">
+                Kanserden öleceğini öğrenen bir kimya öğretmeni, ailesinin geleceğini garanti altına almak için metanfetamin üretip satmak üzere eski bir öğrencisiyle kafa kafaya verir.
+            </div>
+            <div className="button-container">
+                <button>Oynat</button>
+                <button>Daha Fazla Bilgi</button>
+            </div>
+            <span className="maturity-rating">+18</span>
         </div>
     )
 }
