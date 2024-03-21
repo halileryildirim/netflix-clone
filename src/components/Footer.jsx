f8794b54b1c288d17f1328ae11bcc677a4dbcdb5
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-22 01:47:09 +0300

    Refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,18 @@ const Footer = () => {
                 <li>Youtube</li>
             </ul>
             <div className="footer-links">
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
+                <li><a href="#">Sesli Betimleme</a></li>
+                <li><a href="#">Yatırımcı İlişkileri</a></li>
+                <li><a href="#">Yasal Hükümler</a></li>
+                <li><a href="#">Yardım Merkezi</a></li>
+                <li><a href="#">İş İmkanları</a></li>
+                <li><a href="#">Çerez Tercihleri</a></li>
+                <li><a href="#">Hediye Kartları</a></li>
+                <li><a href="#">Kullanım Koşulları</a></li>
+                <li><a href="#">Kurumsal Bilgiler</a></li>
+                <li><a href="#">Medya Merkezi</a></li>
+                <li><a href="#">Gizlilik</a></li>
+                <li><a href="#">Bize Ulaşın</a></li>
             </div>
             <button>Hizmet Kodu</button>
             <p>Copyright info</p>
