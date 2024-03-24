3cc023c1b7713cace217340aa540b6b629a40e43
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-24 03:12:56 +0300

    Refactor footer css

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -2,21 +2,24 @@ import { useState } from "react";
 import { useEffect } from "react"
 
 const API = () => {
-    const [api, setApi] = useState(null);
-    const url = 'https://api.themoviedb.org/3/movie/11?api_key=cd48b9b030580909fcce753d6fde8a06';
-    useEffect(() => {
-        async function fetchAPI() {
-            try {
-                const response = await fetch(url);
-                const data = await response.json();
-                setApi(data);
-            } catch (error) {
-                console.log(error);
-            }
+    const [api, setApi] = useState([]);
+    const url = "https://api.themoviedb.org/3/movie/popular?api_key=cd48b9b030580909fcce753d6fde8a06";
+    async function fetchAPI() {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setApi(data.results);
+            
+        } catch (error) {
+            console.log(error);
         }
+        
+    }
+    useEffect(() => {
         fetchAPI();
     }, [])
-    console.log(api);
+
+    return {api}
 }
 
 export default API;
