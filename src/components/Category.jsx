a5389a173ec50fec8eb3bb76ddbc11e2d3d98668
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-25 03:32:18 +0300

    Add sliding effect to category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,32 +1,40 @@
 import axios from "./axios";
 import { useEffect, useState } from "react";
+import "../styles/Category.css";
 
 const Category = ({ title, fetchUrl }) => {
-    const [content, setContent] = useState([]);
+  const [content, setContent] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get(fetchUrl);
-                setContent(response.data.results);
-                return response;
-            } catch (error) {
-                console.log(error); 
-            }
-        }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(fetchUrl);
+        setContent(response.data.results);
+        return response;
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-        fetchData();
+    fetchData();
+  }, [fetchUrl]);
 
-    }, [fetchUrl])
-
-    return (
-        <div className="category">
-            <h2>{title}</h2>
-            {content && content.map((content) => (
-                <img key={content.id} src={`https://image.tmdb.org/t/p/w500/${content.backdrop_path}`} alt={content.name}></img>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="category">
+      <h2>{title}</h2>
+      <div className="row-posters">
+        {content &&
+          content.map((content) => (
+            <img
+              className="row-poster"
+              key={content.id}
+              src={`https://image.tmdb.org/t/p/w500/${content.backdrop_path}`}
+              alt={content.name}
+            ></img>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Category;
