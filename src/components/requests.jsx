835a5efa001cc86023a4905220df307959b90204
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-26 02:10:22 +0300

    Add info for content cards

--- src/components/requests.jsx
+++ src/components/requests.jsx
@@ -1,10 +1,10 @@
 const APIKEY = "cd48b9b030580909fcce753d6fde8a06";
 
 const requests = {
-  fetchTrends: `/trending/all/week?api_key=${APIKEY}&language=tr-TR`,
+  fetchTrends: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=tr-TR`,
-  fetchPopular: `/tv/popular?api_key=${APIKEY}&language=tr-TR`,
+  fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+  fetchPopular: `/tv/popular?api_key=${APIKEY}&language=en-US`,
 
   fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
